efd721cbb2929be64d88f989c0cddf82e804b020
cloudzfy <cloudzfy@gmail.com>
2013-08-23 21:07:33 +0800

    Copy project from Google Code.

---
+++ doctor_client/ECHelper2.0/ECHelper2.0/App.xaml.cs
@@ -0,0 +1,179 @@
+﻿/*
+ * echelper, An Open Source Mobile Medical System
+ * Copyright (C) 2013, Nicefforts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ECHelper2._0
+{
+    public partial class App : Application
+    {
+        /// <summary>
+        ///提供对电话应用程序的根框架的轻松访问。
+        /// </summary>
+        /// <returns>电话应用程序的根框架。</returns>
+        /// 
+
+        public MailDataContract selectedPatient { get; set; }
+
+        public EmergencyMesg patientlocation;
+
+        public List<MailDataContract> checkedmail { get; set; }
+
+        public PatientUserDataContract PatientDescription {get; set;}
+
+        public MailDataContract selectedRecord { get; set; }
+
+
+        public PhoneApplicationFrame RootFrame { get; private set; }
+
+        //在此处定义应用于全局的各类参数，如医生的ID
+
+        public String DoctorID;
+
+
+        /// <summary>
+        /// Application 对象的构造函数。
+        /// </summary>
+        public App()
+        {
+            // 未捕获的异常的全局处理程序。 
+            UnhandledException += Application_UnhandledException;
+
+            // 标准 Silverlight 初始化
+            InitializeComponent();
+
+            // 特定于电话的初始化
+            InitializePhoneApplication();
+
+            // 调试时显示图形分析信息。
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // 显示当前帧速率计数器。
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                // 显示在每个帧中重绘的应用程序区域。
+                //Application.Current.Host.Settings.EnableRedrawRegions = true；
+
+                // 启用非生产分析可视化模式， 
+                // 该模式显示递交给 GPU 的包含彩色重叠区的页面区域。
+                //Application.Current.Host.Settings.EnableCacheVisualization = true；
+
+                // 通过将应用程序的 PhoneApplicationService 对象的 UserIdleDetectionMode 属性
+                // 设置为 Disabled 来禁用应用程序空闲检测。
+                //  注意: 仅在调试模式下使用此设置。禁用用户空闲检测的应用程序在用户不使用电话时将继续运行
+                // 并且消耗电池电量。
+                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            }
+
+        }
+
+        // 应用程序启动(例如，从“开始”菜单启动)时执行的代码
+        // 此代码在重新激活应用程序时不执行
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+        }
+
+        // 激活应用程序(置于前台)时执行的代码
+        // 此代码在首次启动应用程序时不执行
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+        }
+
+        // 停用应用程序(发送到后台)时执行的代码
+        // 此代码在应用程序关闭时不执行
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        // 应用程序关闭(例如，用户点击“后退”)时执行的代码
+        // 此代码在停用应用程序时不执行
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+        }
+
+        // 导航失败时执行的代码
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // 导航已失败；强行进入调试器
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        // 出现未处理的异常时执行的代码
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // 出现未处理的异常；强行进入调试器
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        #region 电话应用程序初始化
+
+        // 避免双重初始化
+        private bool phoneApplicationInitialized = false;
+
+        // 请勿向此方法中添加任何其他代码
+        private void InitializePhoneApplication()
+        {
+            if (phoneApplicationInitialized)
+                return;
+
+            // 创建框架但先不将它设置为 RootVisual；这允许初始
+            // 屏幕保持活动状态，直到准备呈现应用程序时。
+        //    RootFrame = new PhoneApplicationFrame();
+            RootFrame = new TransitionFrame();
+            RootFrame.Navigated += CompleteInitializePhoneApplication;
+
+            // 处理导航故障
+            RootFrame.NavigationFailed += RootFrame_NavigationFailed;
+
+            // 确保我们未再次初始化
+            phoneApplicationInitialized = true;
+        }
+
+        // 请勿向此方法中添加任何其他代码
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            // 设置根视觉效果以允许应用程序呈现
+            if (RootVisual != RootFrame)
+                RootVisual = RootFrame;
+
+            // 删除此处理程序，因为不再需要它
+            RootFrame.Navigated -= CompleteInitializePhoneApplication;
+        }
+
+        #endregion
+    }
+}
